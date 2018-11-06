c39f98206cf100b781b063d9e56cb1c2653424f8
Carmine Caiazzo <KarmineKaiazzo11@gmail.com>
2018-11-06 14:01:02 +0000

    bozza programma massimo

---
+++ massimo.py
@@ -0,0 +1,19 @@
+#Programma che individua il massimo tra 10 interi.
+
+i = 0
+
+N = input('Inserisci 1o valore:  ')
+N = int(N)
+M = N
+
+while  i<9
+
+ N = input('Inserisci ' i 'o valore:  ')
+ N = int(N)
+ M = N
+ 
+
+
+     print i 
+     i += 1 
+      
